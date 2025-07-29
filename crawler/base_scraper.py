ba92e5e2cb7cd9b8ea87d674b656c45520713cc0
123 <154724648+zmzabc@users.noreply.github.com>
2025-07-29 20:36:42 +0800

    Update base_scraper.py

--- crawler/base_scraper.py
+++ crawler/base_scraper.py
@@ -28,5 +28,5 @@ class BaseScraper(ABC):
         self.model = os.getenv("OPENAI_MODEL")
 
         self.headers = {
-            'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/138.0.0.0 Safari/537.36 Edg/138.0.0.0'
+            'User-Agent':'XXXXXXXXXXXX'
             }
